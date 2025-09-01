e215705a872e17030552d5ffa3a8351b429e5558
lu3eskywalker <surajjhajhria1@gmail.com>
2025-09-01 17:00:30 +0000

    adding a janky version of the flappy bird that mints tokens

--- FlappyBird/Assets/PauseMenu.cs
+++ FlappyBird/Assets/PauseMenu.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+using UnityEngine.Networking;
+using System.Collections;
+using System.Text;
+
+
+class AcceptAllCertificatesSignedHandler : CertificateHandler
+{
+    protected override bool ValidateCertificate(byte[] certificateData)
+    {
+        return true; // Accept any cert
+    }
+}
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pausePanel;
@@ -9,6 +21,8 @@ public class PauseMenu : MonoBehaviour
     public Button resumeButton;
     public TMP_InputField walletInputField; // input field for wallet
     public Button claimButton; // claim button
+
+    public GameManager gameManager;
 
     private void Start()
     {
@@ -34,13 +48,43 @@ public class PauseMenu : MonoBehaviour
     public void ClaimReward()
     {
         string walletAddress = walletInputField.text;
+
         if (string.IsNullOrEmpty(walletAddress))
         {
             Debug.Log("Please enter a wallet address.");
         }
         else
         {
-            Debug.Log("Claiming reward for wallet: " + walletAddress);
+            int currentScore = int.Parse(gameManager.scoreText.text);
+            Debug.Log("Claiming reward for wallet: " + walletAddress + " with score: " + currentScore);
+
+            string jsonData = $"{{\"userAddressFromRequest\":\"{walletInputField.text}\", \"tokenToMint\":\"{currentScore}\"}}";
+            StartCoroutine(PostRequestForBackendReward("http://localhost:3000/api/v1/backendrequest", jsonData));
         }
     }
+
+    public IEnumerator PostRequestForBackendReward(string uri, string jsonData)
+    {
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+        using (UnityWebRequest webRequest = new UnityWebRequest(uri, "POST"))
+        {
+            webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError ||
+                webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Response: " + webRequest.downloadHandler.text);
+            }
+        }
+
+    }
+
 }
